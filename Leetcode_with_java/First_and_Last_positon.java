ec89678f0617615b7929e1054b49da03ae5ad321
Kakrote <anshul.pundir111@gmail.com1>
2024-03-10 22:11:33 +0530

    dfdfdf

---
+++ Leetcode_with_java/First_and_Last_positon.java
@@ -0,0 +1,45 @@
+/**
+ * First_and_Last_positon
+ */
+public class First_and_Last_positon {
+
+    public static void main(String[] args) {
+        int arr[] = {5, 7, 7, 7, 8, 9, 9, 9, 10};
+        int target = 9;
+        int a[] = position(arr, target);
+        System.out.println(java.util.Arrays.toString(a));
+
+    }
+
+    public static int[] position(int[] arr, int target) {
+        int[] ans = {-1, -1};
+        int start = search(arr, target, true);
+        int end = search(arr, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+
+    public static int search(int[] arr, int target, boolean isstart) {
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                if (isstart) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
--- Leetcode_with_java/celling_number.java
+++ Leetcode_with_java/celling_number.java
@@ -1,33 +1,34 @@
-// package Leetcode_with_java;
-
-// celling of a number means if the target element is not in the array then find the nearest greater number in the array.
 public class celling_number {
 
     public static void main(String[] args) {
-        int [] arr={2,3,4,9,14,16,18};
-        int target=15;
-        int a=celling(arr, target);
+        int[] arr = {2, 3, 4, 9, 14, 16, 18};
+        int target = 1;
+        int a = ceiling(arr, target);
         System.out.println(a);
-
     }
-    public static int celling(int []arr,int target){
-        int start=0;
-        int end=arr.length-1;
-        
-        while (start<=end) {
-            int mid=start+(end-start)/2;
-            if(target==arr[mid]){
-                return arr[mid];
-            }            
-            else if(target<arr[mid]){
-                end=mid;
 
-            }
-            else if (target>arr[mid]){
-                start=mid;
+    public static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return arr[mid];
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-        return start;
 
+        // If the target is not found in the array
+        // and start is pointing to the next greater element
+        // or if start is out of bounds, return -1 (indicating not found)
+        if (start < arr.length) {
+            return arr[start];
+        } else {
+            return -1; // Return -1 or any other suitable value
+        }
     }
 }
