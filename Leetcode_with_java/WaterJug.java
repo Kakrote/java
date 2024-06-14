1ca17a81bbb15624277c8438386d21cddc0505af
Kakrote <anshul.pundir111@gmail.com1>
2024-06-14 23:22:51 +0530

    water jug

--- Leetcode_with_java/WaterJug.java
+++ Leetcode_with_java/WaterJug.java
@@ -4,23 +4,24 @@ public class WaterJug {
     }
     // to check if the jugs can fill the target is to check the hcf of the x and y
     public boolean canMeasureWater(int x, int y, int target) {
-        int h = gcf(x, y);
-        // case 1 when x=1 and y=1 and target =1 as (0,1) true 
+        int h=gcd(x, y);
+        //case1 x==y==target 
         if(x==y && y==target){
             return true;
         }
-        // case 2 when x=1 y=1 and target =5 there is no possible way to fill the target 
-        if(x==y){
+        // case 2 x==y y!=target
+        if(x==y && y!=target){
             return false;
         }
-        // case 3 this is the standard case for the rest 
-        if(x!=y && target%h==0){
+        // case 3 this is the standard case
+        if(target%h==0){
             return true;
         }
+
+
         return false;
-      
     }
-    public int gcf(int x,int y){
+    public int gcd(int x,int y){
         while(y!=0){
             int rem=x%y;
             x=y;
