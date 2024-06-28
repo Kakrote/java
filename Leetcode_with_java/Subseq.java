ea1dec28d7f5ffb9b6b71fef378342312ce61e8b
Kakrote <anshul.pundir111@gmail.com1>
2024-06-28 23:56:19 +0530

    using ArrayList

--- Leetcode_with_java/Subseq.java
+++ Leetcode_with_java/Subseq.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Subseq {
     public static void main(String[] args) {
-        subseq("abc", " ");
+        // subseq("abc", " ");
+        System.out.println(subseqlist("abc", ""));
     }
     static void subseq(String str,String ans){// str is the original string and ans will contain the subseq of the original string.
         if(str.isEmpty()){
@@ -11,6 +14,20 @@ public class Subseq {
         char ch=str.charAt(0);
         subseq(str.substring(1),ans+ch);// this will return the function with adding (left part of the tree)
         subseq(str.substring(1),ans);// this will return the function with ingoring (right part of the tree)
-        // subseq(ans,str.substring(1));
+    }
+
+    static ArrayList<String> subseqlist(String str,String ans){
+        if(str.isEmpty()){
+            ArrayList <String>list=new ArrayList<>();
+            list.add(ans);
+            return list;
+
+        }
+        char ch=str.charAt(0);
+        ArrayList<String>left=subseqlist(str.substring(1), ans+ch);// this will have the taking left hand part
+        ArrayList<String>right=subseqlist(str.substring(1), ans);// this will have the ingnoring right hand part.
+        
+        left.addAll(right);
+        return left;
     }
 }
